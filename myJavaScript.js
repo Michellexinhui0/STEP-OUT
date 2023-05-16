a612d950b1077f4e71a22e7302e21b4e360b5321
Michelle Ling <michellexinhui.ling@sd.taylors.edu.my>
2023-05-16 23:35:36 +0800

    16/5 progress

--- myJavaScript.js
+++ myJavaScript.js
@@ -4,3 +4,13 @@ const container = document.getElementById('container');
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+$(document).ready(function() {
+	$('#calendar').datetimepicker(); // Initialize the date picker
+
+	// Prevent closing the dropdown when clicking inside the date picker
+	$('.dropdown-menu').click(function(e) {
+	  e.stopPropagation();
+	});
+  });
+
