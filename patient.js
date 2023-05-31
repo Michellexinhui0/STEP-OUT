09679203f2fe8ffa7cea61616deb9c1c30222537
KuzeCodes <81248273+KuzeCodes@users.noreply.github.com>
2023-05-31 14:35:18 +0000

    31/5 ku update

--- patient.js
+++ patient.js
@@ -1,9 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   console.log("DOM is loaded");
   const save = document.getElementById("Save");
-  console.log(save);
   const details = document.getElementById("basic-details");
-  console.log(details);
   const conditions = document.getElementById("conditions");
   details.style.display = "none";
   conditions.style.display = "grid";
