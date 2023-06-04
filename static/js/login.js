5dbedb22c0161d6b8f7333e8388493507e22f9c2
KeebHub <94605914+KeebHub@users.noreply.github.com>
2023-06-04 12:29:40 +0800

    Added steps to recreate problem

--- static/js/login.js
+++ static/js/login.js
@@ -1,3 +1,13 @@
+//To Recreate the problem uncomment out 1. login.JS ur code leave the bottom one commented
+//login.html oso add back in the method + action
+//serve with flask, the login button will bring to the blank page with user info
+// That is base case
+//Now comment out the entire JS part
+//rerun server it still can do the same thing
+//now comment out the html action + method for the login form
+//now its sending get req and can view user info in the address bar
+
+/*
 const logInButton = document.getElementById("logIn");
 const container = document.getElementById("container");
 
@@ -27,4 +37,27 @@ clickLogin = (e) => {
       }
     });
 };
+*/
 
+/*
+function login(){
+
+  e.preventDefault();
+  const form = document.getElementsByName("login_form")
+  const data = new FormData(form)
+  const loginData = new URLSearchParams(data).toString();
+
+  $.ajax({
+    url: "/user/login",
+    type: "POST",
+    data: loginData,
+    dataType: "json",
+    success: function(resp) {
+      window.location.href = "/search/";
+    },
+    error: function(resp) {
+      $error.text(resp.responseJSON.error).removeClass("error--hidden");
+    }
+  });
+}
+*/
